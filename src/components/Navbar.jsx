48cd133c34d32d5ac1a035f4efc13b8957762aae
nathalymolina99 <nathaly.molina02@inacapmail.cl>
2023-11-09 15:31:49 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -250,7 +250,7 @@ export default function Navbar() {
                                 {item.name}
                               </a>
                               <p aria-hidden="true" className="mt-1">
-                                Shop now
+                                Comprar ahora
                               </p>
                             </div>
                           ))}
