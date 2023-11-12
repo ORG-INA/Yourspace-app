a3e317680bd9d76b30cdcd148ef2f2aa8850d1bc
nathalymolina99 <nathaly.molina02@inacapmail.cl>
2023-11-12 02:08:05 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
 import App from "./app";
 import { AuthProvider } from "./contexts/auth/AuthProvider";
 import './styles.css'; 
