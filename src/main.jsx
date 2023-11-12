d380e157175df9251b6e90199dfdd63983577e6d
nathalymolina99 <nathaly.molina02@inacapmail.cl>
2023-11-12 01:56:17 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 import { AuthProvider } from "./contexts/auth/AuthProvider";
 import './styles.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
