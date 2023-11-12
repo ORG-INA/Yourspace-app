00c1af53be46258b35debfe7da3fabee01be9ad6
nathalymolina99 <nathaly.molina02@inacapmail.cl>
2023-11-12 01:43:04 -0300

    Agregando Elementos
    HomePage, Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./app";
 import { AuthProvider } from "./contexts/auth/AuthProvider";
+import './styles.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
