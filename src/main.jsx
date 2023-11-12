8c6e87620107cca7333de7997d8301ec79e1ccdc
nathalymolina99 <nathaly.molina02@inacapmail.cl>
2023-11-12 01:53:46 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./app";
 import { AuthProvider } from "./contexts/auth/AuthProvider";
 import './styles.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -13,3 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </AuthProvider>
   </React.StrictMode>
 );
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />);
