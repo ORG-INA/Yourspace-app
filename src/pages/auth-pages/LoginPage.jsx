e4eef4bfc8becb8be2ce7f657027bde5c10f4a57
nathalymolina99 <nathaly.molina02@inacapmail.cl>
2023-11-12 05:19:08 -0300

    inicio de sesion

--- src/pages/auth-pages/LoginPage.jsx
+++ src/pages/auth-pages/LoginPage.jsx
@@ -4,32 +4,33 @@ import LoginForm from "../../components/LoginForm";
 export default function Login() {
   return (
     <>
-      <div className="flex flex-wrap w-full">
-        <div className="flex flex-col w-full md:w-1/2">
-          <div className="flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24">
-            <Link to="/" className="p-4 text-xl font-bold text-white bg-black">
-              YourSpace.
-            </Link>
-          </div>
-          <div className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
-            <p className="text-3xl text-center">Bienvenido.</p>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 mt-5">
+            <div className="flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24">
+              <Link to="/" className="p-4 text-xl font-bold text-white bg-black">
+                YourSpace.
+              </Link>
+            </div>
+            <br/>
+            <br/>
+            <h1 className="text-center mb-4">Bienvenido.</h1>
             <LoginForm />
-            <div className="pt-12 pb-12 text-center">
+            <div className="pt-12 text-center">
+              <br/>  
               <p>
                 ¿No tienes cuenta aún?
-                <Link to="/register" className="font-semibold underline ml-1">
+                <Link to="/register" className="text-dark">
                   Registrate aquí.
                 </Link>
               </p>
             </div>
           </div>
+          <div className="col-md-6">
+            <img src="yourspace-login.jpg" alt="Imagen de Registro" className="img-fluid" />
+          </div>
         </div>
-        <div className="w-1/2 shadow-2xl">
-          <img
-            className="hidden object-cover w-full h-screen md:block contrast-75"
-            src="/yourspace-login.jpg"
-          />
-        </div>
+        
       </div>
     </>
   );
