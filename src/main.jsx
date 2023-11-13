dfecf1d3caccac3459e75deb9f28492724fe6408
Enrique Ansa <enriqueansa@gmail.com>
2023-11-13 18:51:25 -0300

    fixed: imprtacion de CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./app";
 import { AuthProvider } from "./contexts/auth/AuthProvider";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CartProvider } from "./contexts/shoppingCart/cartProvider";
+import { CartProvider } from "./contexts/shoppingCart/CartProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
